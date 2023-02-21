3cd79641a0933d786002b8f8402662e56cf591db
Wassim <wassimchakib97@gmail.com>
2023-02-21 11:01:52 +0100

    Setup database

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module BlogApp
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 

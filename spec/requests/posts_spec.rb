4ce65c0f61342496fd0fea0b7f9401ddbcffd475
Wassim <wassimchakib@hotmail.com>
2023-03-02 17:14:39 +0100

    Remove usemove boilerplate testing code

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,21 +3,6 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'GET /users/:user_id/posts' do
     before(:each) do
-      @user = User.create(
-        name: 'Wassim Chakib',
-        photo: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-        bio: 'Software engineer',
-        postsCounter: 0
-      )
-
-      @post = Post.create(
-        author: @user,
-        title: 'My first post',
-        text: 'This is my first post in my page where I will talk about software development!',
-        commentsCounter: 5,
-        likesCounter: 47
-      )
-
       get '/users/1/posts'
     end
 
@@ -40,21 +25,6 @@ RSpec.describe 'PostsController', type: :request do
 
   describe 'GET /users/:user_id/posts/:id' do
     before(:each) do
-      @user = User.create(
-        name: 'Wassim Chakib',
-        photo: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-        bio: 'Software engineer',
-        postsCounter: 0
-      )
-
-      @post = Post.create(
-        author: @user,
-        title: 'My first post',
-        text: 'This is my first post in my page where I will talk about software development!',
-        commentsCounter: 5,
-        likesCounter: 47
-      )
-
       get '/users/1/posts/2'
     end
 

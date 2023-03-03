035e764854656e4c70f888e0999f44557fb2df50
Wassim <wassimchakib97@gmail.com>
2023-03-03 23:47:33 +0100

    Add posts controller functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(author_id: params[:user_id], id: params[:id]) || "No posts found for User #{params[:id]}"
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(person_params)
+    if @post.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(author: current_user)
+  end
 end

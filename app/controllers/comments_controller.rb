67d1ffaaad969a3638088dd531fcccb60846808d
Wassim <wassimchakib@hotmail.com>
2023-03-04 01:43:58 +0100

    Fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.author_id = current_user.id
     @comment.post_id = params[:post_id]
-    post = Post.find(params[:post_id])
     if @comment.save
       redirect_to user_posts_path(user_id: params[:user_id])
     else
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-  
+
     if @post.save
       redirect_to users_path
     else

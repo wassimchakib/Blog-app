a60bc6855efcbc4ede05f6914381a278a7247ff2
Wassim <wassimchakib@hotmail.com>
2023-03-07 21:38:52 +0100

    Refactor post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,41 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  user = User.create(name: 'john', photo: 'link to photo', bio: 'dev', postsCounter: 0)
-
-  subject do
+  let(:user) { User.new(name: 'john', photo: 'link to photo', bio: 'dev', postsCounter: 0) }
+  let(:post) do
     Post.new(author: user, title: 'web dev best practices', text: 'this is where the post text goes',
              commentsCounter: 0, likesCounter: 0)
   end
-
-  it 'title should be present' do
-    subject.title = nil
-    expect(subject).to_not be_valid
+  let(:comment) { Comment.new(post:, author: user, text: 'test') }
+  it 'fails because title should be present' do
+    post.title = nil
+    expect(post).to_not be_valid
   end
 
-  it 'comments count should be integer' do
-    subject.commentsCounter = 'hello'
-    expect(subject).to_not be_valid
+  it 'fails because comment counter should be an integer value' do
+    post.commentsCounter = 'hello'
+    expect(post).to_not be_valid
   end
 
-  it 'comments count should be >= 0' do
-    subject.commentsCounter = -1
-    expect(subject).to_not be_valid
+  it 'fails because comment counter should be >= 0' do
+    post.commentsCounter = -1
+    expect(post).to_not be_valid
   end
 
-  it 'likes count should be integer' do
-    subject.likesCounter = 'hello'
-    expect(subject).to_not be_valid
+  it 'fails because like counter should be an integer' do
+    post.likesCounter = 'hello'
+    expect(post).to_not be_valid
   end
 
-  it 'likes count should be >= 0' do
-    subject.likesCounter = -1
-    expect(subject).to_not be_valid
+  it 'fails because like counter should be >= 0' do
+    post.likesCounter = -1
+    expect(post).to_not be_valid
   end
 
-  it 'returns recent comments' do
-    Comment.create(post: subject, author: user, text: 'test')
-    comments = subject.recent_comments
-    expect(comments.length).to eq 1
+  it 'should return 1 since User added 1 comment to the post' do
+    comment.update_comments_counter
+    expect(post.commentsCounter).to eq 1
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.new(name: 'john', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'dev', postsCounter: 0) }
-  let(:post) { Post.new(author: user, title: 'My first blogpost', text: 'Hello, today I will talk about my experience.', commentsCounter: 0, likesCounter: 0) }
+  let(:post) do
+    Post.new(author: user, title: 'My first blogpost', text: 'Hello, today I will talk about my experience.',
+             commentsCounter: 0, likesCounter: 0)
+  end
   it 'fails because name should be present' do
     user.name = nil
     expect(user).to_not be_valid

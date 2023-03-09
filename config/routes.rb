a8363c893143de7ea872f3e0e11606daf1be211c
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-09 14:34:40 +0300

    Adds styling and updates routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show] do
       resources :comments, only: [:new, :create]
